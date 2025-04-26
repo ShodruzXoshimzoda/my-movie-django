f93f3050afec3cfcb7cbe6f7222584b19fa98ca8
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-26 17:44:36 +0500

    Создал функциональное представление для фильмов

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.views.generic import ListView
+from .models import Movie
 
+'''  CBV  '''
 
 def index(request):
-    return render(request, 'movie/movies.html')
+    movie_list = Movie.objects.all()
+    context= {'movie_list':movie_list}
+    return render(request,'movie/movies.html',context)
+
+def movie_detail(request,slug):
+    movie = Movie.objects.get(url=slug)
+    context = {'movie':movie}
+
+    return render(request,'movie/moviesingle.html',context)
+
