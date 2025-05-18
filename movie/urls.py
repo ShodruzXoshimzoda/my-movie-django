4a1c99538c052841d9ec06091dbee34515a4eec7
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-05-18 18:44:17 +0500

    Перевёл из функционального представление в классовое представсление

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from .views import index,movie_detail
+# from .views import index,movie_detail
+from . import views
 
 urlpatterns = [
-    path("",index),
-    path("<slug:slug>/",movie_detail,name='movie_detail'),
-
+    path("",views.MoviesView.as_view()),
+    path("<slug:slug>/",views.MovieDetailView.as_view(),name='movie_detail'),
 
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Movie
 
 '''  CBV  '''
 
-def index(request):
-    movie_list = Movie.objects.all()
-    context= {'movie_list':movie_list}
-    return render(request,'movie/movies.html',context)
+# def index(request):
+#     movie_list = Movie.objects.all()
+#     context= {'movie_list':movie_list}
+#     return render(request,'movie/movies.html',context)
 
-def movie_detail(request,slug):
-    movie = Movie.objects.get(url=slug)
-    context = {'movie':movie}
+# def movie_detail(request,slug):
+#     movie = Movie.objects.get(url=slug)
+#     context = {'movie':movie}
 
-    return render(request,'movie/moviesingle.html',context)
+#     return render(request,'movie/moviesingle.html',context)
 
+class MoviesView(ListView):
+    ''' Spisok filmov'''
+    model = Movie
+    queryset = Movie.objects.filter(draft = False)
+    template_name = 'movie/movies.html'
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
